5fa76325749fc112b0cded07e116922c4ce36c1d
Denis Malkin <malkin.de@pm.me>
2025-03-25 21:20:09 +0100

    Add type to AgendaSource; add AgendaSource API.

--- backend/models/AgendaSource.go
+++ backend/models/AgendaSource.go
@@ -9,6 +9,7 @@ type AgendaSource struct {
 	gorm.Model
 	ResourceID  uuid.UUID `gorm:"type:uuid;default:gen_random_uuid()"`
 	Url         string
+	Type        string
 	UserID      uint
 	AgendaItems []AgendaItem `gorm:"constraint:OnDelete:SET NULL;"`
 }
